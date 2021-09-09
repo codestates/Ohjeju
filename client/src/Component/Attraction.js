cef1ad06e1de42ea1d82795d77213c99705e8fdc
Capy-0 <yongsog@gmail.com>
2021-09-09 15:57:33 +0900

    client setting

---
+++ client/src/Component/Attraction.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect} from "react";
+import { Link } from "react-router-dom"
+import axios from "axios"
+import '../css/Attraction.module.css'
+
+require('dotenv').config();
+
+const Attraction = () => {
+
+    return (
+        <div>
+            명소
+        </div>
+    )
+}
+
+export default Attraction;
---
+++ client/src/Component/Planner.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect} from "react";
+import { Link } from "react-router-dom"
+import axios from "axios"
+import '../css/Planner.module.css'
+
+require('dotenv').config();
+
+const Planner = () => {
+
+    return (
+        <div>
+            플래너 페이지
+        </div>
+    )
+}
+
+export default Planner;
