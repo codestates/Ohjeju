f8005d89290299159a05a664b8ce490f004afbc7
Capy-0 <yongsog@gmail.com>
2021-09-13 14:28:41 +0900

    logmodal

--- client/src/Component/SignupModal.js
+++ client/src/Component/SignupModal.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import {React, useState} from 'react';
+import axios from 'axios';
+import '../css/SignupModal.css'
 
 export default function SignupModal() {
 
+    const [showSignupModal , setshowSignupModal] = useState(false);
+
+    return (
+        <signup>
+        <span id='header_SignUp' onClick={setshowSignupModal}>
+          회원가입
+        </span>
+        {showSignupModal? (
+            <div className='popup'>
+                <div className='popup_inner'>
+                </div>
+            </div>
+        ):null}
+        </signup>
+      )
 }
