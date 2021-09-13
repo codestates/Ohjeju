8a941bf9c4dfcdac8548f1d0a718e33685c29be4
MinJiK1912 <hibabel1219@gmail.com>
2021-09-13 14:36:33 +0900

    [SignController.js] / 로그인 관련 함수 구현 토큰 관리 함수 템플릿 별도로 구현해서 추가하겠습니다.

--- server/controller/SignController.js
+++ server/controller/SignController.js
@@ -1,62 +1,81 @@
-const {attractions,group,plan,planner,reviews,users,user_group} = require('../models')
-//필요한 모델 require 해와야 함
+const Users = require('../models').users;
 const jwt = require('jsonwebtoken');
 
 //유저 로그인 상태 관련 method
 module.exports = {
   signIn: async (req, res) => {
     //* endpoint: https://www.Ohjeju.com/signin
-    try{
-      const findUser = await users.findOne({
-        where:{
-          email:req.body.email,
-          password:req.body.password
+    try {
+      const findUser = await Users.findOne({
+        where:  {
+          email: req.body.email,
+          password: req.body.password
         }
       })
-      if(findUser !== null){
-        res.status(200).send('ok')
-      }
-      else{
-        res.status(400).send('not authorized')
-      }
+
+      const { id, email, userName } = findUser;
+      const tokenPayload = { id, email, userName };
+
+      const accessToken = jwt.sign(tokenPayload, process.env.ACCESS_SECRET, { expiresIn : '1h' });
+      const refreshToken = jwt.sign(tokenPayload, process.env.REFRESH_SECRET, { expiresIn : '14d' });
+
+      if(!findUser) return res.status(400).send('not authorized');
+      else return res.status(200)
+        .cookie('accessToken', accessToken)
+        .cookie('refreshToken', refreshToken)
+        .send({
+          "id" : id
+        });
     }
-    catch(err) {res.status(500).send('server error')}
+    catch(err) { return res.status(500).send('server error') }
     //이후토큰처리문제는 얘기
   },
   
   signUp: async (req, res) => {
     //* endpoint: https://www.Ohjeju.com/signup
-    try{
-      const findUser = await users.findOne({
-        where:{email:req.body.email}
+
+    try {
+      const findUser = await Users.findOne({
+        where : { email : req.body.email }
       })
-      if(findUser !== null){ //해당 email가진 유저가 존재할때
-        res.status(409).send("this email already exists")
-      }
-      else{ //위에 중복유저가 없을때 -> 가입절차
-        if(!req.body.email || !req.body.password){ //둘중하나없을때(required한 부분)
-          res.status(400).send('no content')
-        }
-        else{ //둘다채웟을때
-          users.create({
-            email:req.body.email,
-            password:req.body.password,
-            userName:'Default'
-            //image , userName은 마이페이지에서 수정하도록 한다고했음 맨처음 userName은 디폴틀값으로 들어갈듯
+
+      //해당 email을 가진 유저가 이미 존재할 때
+      if(findUser) res.status(409).send('this email is already exists');
+      else { //해당 email로 가입이 되어있지 않으면 -> 가입절차 진행
+        //email이나 password가 입력되어있지 않은 경우
+        if(!req.body.email || !req.body.password) return res.status(400).send('no content');
+        else { //required한 정보가 정상적으로 입력이 되어있는 경우
+          Users.create({
+            email: req.body.email,
+            password: req.body.password,
+            userName: req.body.userName || 'Default',
+            image: req.body.image || null
           })
-          .then(item => {
-            res.status(201).send('ok')
-          })
+
+          return res.status(201).send('ok');
         }
       }
-      
     }
-    catch(err){res.status(500).send('server error')}
+    catch(err) { return res.status(500).send('server error') }
   },
 
   signOut: async (req, res) => {
     //* endpoint: https://www.Ohjeju.com/signout
+    //signOut부분은 토큰 관리 함수 템플릿 만들고나서 다시 바꿔둘게요
 
-    //토큰없애기
+    //토큰 확인해서 해당 토큰이 유효할 경우에만 로그아웃
+    try {
+      const reqAccessToken = req.cookies.accessToken;
+      const reqRefreshToken = req.cookies.refreshToken;
+
+      console.log(`Access: ${reqAccessToken}, Refresh: ${reqRefreshToken}`);
+      const userData = jwt.verify(reqAccessToken, process.env.ACCESS_SECRET);
+      if(userData) return res.status(200)
+        .clearCookie('accessToken')
+        .clearCookie('refreshToken')
+        .send('sign out successfully')
+    }
+    //catch에서 토큰이 유효하지 않은 경우도 잡히는데 API에 401같은거 있어야될거같아요 invalid token으로
+    catch(err) { return res.status(500).send('server error') }
   }
 }
