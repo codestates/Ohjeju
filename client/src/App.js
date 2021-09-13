e0961622f51c04858b0e940b007c6e1f6c7c5477
Lee-Sangkwon1 <opsizks4@gmail.com>
2021-09-13 09:14:10 +0900

    feature/main

--- client/src/App.js
+++ client/src/App.js
@@ -22,16 +22,16 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isOn, setisOn] = useState(false);
 
-  useEffect(() => {
-    scrollStop();
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
+  // useEffect(() => {
+  //   scrollStop();
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 3000);
+  // }, []);
 
-  useEffect(() => {
-    scrollStop();
-  }, [isLoading]);
+  // useEffect(() => {
+  //   scrollStop();
+  // }, [isLoading]);
 
   const scrollStop = () => {
     if (isLoading) {
@@ -47,9 +47,8 @@ function App() {
 
   return (
     <BrowserRouter>
-      {isLoading ? <Loading /> : null}
+      {/* {isLoading ? <Loading /> : null} */}
       <Header isOn={isOn} toggleHandler={toggleHandler} />
-      {/* {isLoading ? <Header /> : null} */}
       <Switch>
         <Route exact path="/">
           <Main />
