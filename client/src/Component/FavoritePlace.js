fa5577faae5a031bc852a88eb912ea06406a179e
Lee-Sangkwon1 <opsizks4@gmail.com>
2021-10-01 13:47:15 +0900

    feature/main

--- client/src/Component/FavoritePlace.js
+++ client/src/Component/FavoritePlace.js
@@ -9,39 +9,61 @@ import logo2 from "../Imgs/logo2.png";
 import logo3 from "../Imgs/logo3.png";
 
 function FavoritePlace({ getPlace, placeList, setPlaceList }) {
+  console.log(placeList);
   const history = useHistory();
   const SERVER_URL = process.env.SERVER_URL || "https://localhos:80";
 
-  const [slide, setSlide] = useState(false);
+  const [count, setCount] = useState(-100);
+  const [slide, setSlide] = useState({
+    transform: `translate(${0}+vw)`,
+  });
 
-  const slideHandler = () => {
-    setSlide(!slide);
+  const slideNextHandler = () => {
+    setSlide({
+      transform: `translate(${count}vw)`,
+    });
+    setCount(count - 100);
   };
+
+  const slideBeforeHandler = () => {};
 
   return (
     <div className="favorite">
       <div className="favorite_title">추천명소</div>
-      <div className="product-container">
+      <div className="bigContainer">
         {placeList.map((item, index) => {
           return (
-            <div className="product-item">
-              <Link
-                to={{
-                  pathname: `/attraction`,
-                  state: {
-                    info: item.info,
-                    image: item.image,
-                    name: item.name,
-                  },
-                }}
-              >
-                <div className="product-all">
-                  <img src={item.image} alt="" key={index} />
-                  <p className="product-p">{item.name}</p>
-                </div>
-              </Link>
-              {/* <div className="product-div">{item.info}</div> */}
+            <div className="middleContainer">
+              <button className="slideNextBtn" onClick={slideBeforeHandler}>
+                before
+              </button>
+              <div className="container">
+                <img src={item.image} className="main-img" style={slide} />
+              </div>
+              <button className="slideBeforeBtn" onClick={slideNextHandler}>
+                next
+              </button>
+              <div className="slideInfo">{item.info}</div>
             </div>
+
+            // <div className="product-item">
+            //   <Link
+            //     to={{
+            //       pathname: `/attraction`,
+            //       state: {
+            //         info: item.info,
+            //         image: item.image,
+            //         name: item.name,
+            //       },
+            //     }}
+            //   >
+            //     <div className="product-all">
+            //       <img src={item.image} alt="" key={index} />
+            //       <p className="product-p">{item.name}</p>
+            //     </div>
+            //   </Link>
+            //   {/* <div className="product-div">{item.info}</div> */}
+            // </div>
           );
         })}
       </div>
