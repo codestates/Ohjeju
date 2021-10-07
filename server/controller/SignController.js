96b1fedfba7933159b3a9be90b924d7851174aeb
MinJiK1912 <hibabel1219@gmail.com>
2021-10-08 03:55:47 +0900

    [SignController] 쿠키 도메인 옵션 수정

--- server/controller/SignController.js
+++ server/controller/SignController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const { verifyToken, decodeToken } = require('./VerifyToken');
 const COOKIE_OPTIONS = {
-  domain: 'oh-jeju.ml',
+  domain: '.oh-jeju.ml',
   path: '/',
   httpOnly: true,
   secure: true,
