4b06797ae719e06596c9310b7dd568661e6006e4
Capy-0 <yongsog@gmail.com>
2021-10-08 03:56:58 +0900

    fix

--- client/src/Component/KakaoMap.js
+++ client/src/Component/KakaoMap.js
@@ -438,6 +438,7 @@ function KakaoMap({ userInfo, plannerInfo, handleDestination, planner1}) {
         </div>
         <div id="chat_wrap" className="bg_white close">
           <Chat plannerInfo={plannerInfo} userInfo={userInfo} />
+          {/* 추가 */}
           <hr></hr>
           <ul id="placesList"></ul>
         </div>
