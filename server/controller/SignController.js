b63c0b5ffd7fe895054d70f0010d1eec53847fca
MinJiK1912 <hibabel1219@gmail.com>
2021-10-08 14:33:50 +0900

    쿠키 옵션 수정 domain, path 옵션 삭제

--- server/controller/SignController.js
+++ server/controller/SignController.js
@@ -4,8 +4,8 @@ const jwt = require('jsonwebtoken');
 
 const { verifyToken, decodeToken } = require('./VerifyToken');
 const COOKIE_OPTIONS = {
-  domain: '.oh-jeju.ml',
-  path: '/',
+  //domain: 'oh-jeju.ml',
+  //path: '/',
   httpOnly: true,
   secure: true,
   sameSite: 'none'
