9065a060bb89caa278e83ed09e95fb4b052903c0
Capy-0 <yongsog@gmail.com>
2021-10-08 19:49:27 +0900

    인증오류 수정

--- client/src/Component/GoogleOAuth.js
+++ client/src/Component/GoogleOAuth.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router";
 import axios from 'axios';
 
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost:80';
-const GOOGLE_LOGIN_PASSWORD = process.env.GOOGLE_LOGIN_PASSWORD;
+const GOOGLE_LOGIN_PASSWORD = process.env.GOOGLE_LOGIN_PASSWORD || 'Z29vZ2xlIGxvZ2luIHBhc3N3b3Jk'
 
 function GoogleOAuth({setuserInfo, setisLogin, getuserInfo}){
 
